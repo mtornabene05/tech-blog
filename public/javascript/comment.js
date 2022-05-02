3c239aed242124f278f102d0e3bd7b417c744fbc
Maria <mtornabene05@gmail.com>
2022-05-02 00:53:28 -0400

    addition of public javascript files

---
+++ public/javascript/comment.js
@@ -0,0 +1,31 @@
+async function commentFormHandler(event) {
+    event.preventDefault();
+
+    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+
+    //wrapped in if statement prevents users from submitting empty comment strings
+    if (comment_text) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if(response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
